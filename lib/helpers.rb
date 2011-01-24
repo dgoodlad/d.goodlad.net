e5d4d4af4a148fcbd217577369efed8bc4d8595a
David Goodlad <david@goodlad.ca>
2011-01-25 07:47:17 +1100

    Start adding archives pages

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,3 +1,25 @@
 include Nanoc3::Helpers::XMLSitemap
 include Nanoc3::Helpers::Blogging
 
+def body_class
+  case @item[:kind]
+  when 'article' then 'article'
+  when 'archives' then 'archives'
+  else 'home'
+  end
+end
+
+def articles_by_year
+  grouped = articles.group_by { |a| Time.parse(a[:created_at]).year }
+  if block_given?
+    grouped.keys.sort.reverse.each do |year|
+      yield [year, grouped[year]]
+    end
+  end
+  p grouped
+  grouped
+end
+
+def article_years
+  articles.map { |a| Time.parse(a[:created_at]).year }.uniq.sort
+end
